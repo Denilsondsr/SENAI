8f5363e8b4ad7a73530d598a58adfd68c604475b
Denilson <dsr@dsr>
2024-10-10 09:45:51 -0300

    JAVASCRIPT - AULA 01

---
+++ IniciandoJavaScript/script.js
@@ -0,0 +1,50 @@
+// JAVASCRIPT - AULA 01
+
+// Exercício 1: Criando Variáveis Simples
+
+// let nome = "Denilson";
+// let idade = 26;
+// let cidade = "São Paulo";
+// console.log(nome);
+// console.log(idade);
+// console.log(cidade);
+
+// Exercício 2: Operações Matemáticas Simples
+// let num1 = 10;
+// let num2 = 10;
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+
+// Exercício 3: Resto da Divisão
+// let num = 10;
+// console.log(num % 2 == 0 ? "Número Par" : "Número Impar");
+
+// Exercício 4: Cálculo de Desconto
+// let precoProd = 500;
+// console.log(precoProd * 0.8);
+
+// Exercício 5: Área de um Retângulo
+// let comprimento = 10;
+// let largura = 10;
+// let area = comprimento * largura;
+// console.log(area);
+
+// EXERCÍCIO 16.1
+
+// let salarioBruto = 3000;
+// let imposto = 10;
+// let despesas = 100;
+// let bonus = 100;
+// let temPlanoSaude = true;
+// let valorImposto = (salarioBruto * imposto) / 100;
+// let salarioLiquido = salarioBruto - valorImposto - despesas + bonus;
+
+// if (temPlanoSaude) {
+//   salarioLiquido -= 300;
+// }
+
+// let resultado = `Salario Liquido: R$ ${salarioLiquido.toFixed(2)}`;
+
+// console.log(resultado);
