11f232b9123a7860bbc9d7f1149aa42bd53d3ab6
Denilson <dsr@dsr>
2024-10-16 15:35:27 -0300

    pra cima

--- IniciandoPython/main.py
+++ IniciandoPython/main.py
@@ -1,3 +1,15 @@
 import tkinter as tkter
+
+def dizerOla():
+  rotulo.config(text="Olá, mundo!")
+
 janela = tkter.Tk()
+janela.title("Dizer Olá")
+
+botaoDizer = tkter.Button(janela, text="Olá, mundo!", command=dizerOla)
+botaoDizer.pack(pady=20)
+
+rotulo = tkter.Label(janela,text="")
+rotulo.pack()
+
 janela.mainloop()
